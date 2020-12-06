fc3d616f922d8f40e0f818a20f0abcd5117e39ec
Necmettin <necmettin.cimen@alpata.com>
2020-12-06 14:46:53 +0300

    first

--- Models/Database/Event.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace TaskManager.Models
-{
-    public class Event : BaseEntity
-    {
-        [Required] [MaxLength(250)] public string Name { get; set; }
-    }
-}
--- Models/Database/Label.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace TaskManager.Models
-{
-    public class Label : BaseEntity
-    {
-        [Required] [MaxLength(250)] public string Name { get; set; }
-    }
-}
